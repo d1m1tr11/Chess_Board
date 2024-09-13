96d8e3d6cf2a182c03e9f7b06f1fcc9dce5304d2
Dimitri <dkakhniashvili@DKAKHNIASHVILI>
2024-09-13 12:24:35 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int indeqsi = 0;
+            for (int i=0; i<64;i++)
+            {
+                if (i % 8 == 0 && i>0)
+                {
+                    Console.WriteLine();
+                    indeqsi++;
+                }
+                if (indeqsi % 2 ==0)
+                {
+                    Console.Write("|Tetri| ");
+                }
+                else { Console.Write("|Shavi| ");
+                }
+
+                indeqsi++;
+            }
+            
+        }
+    }
+}
